a34de3abc1b6cf9cb82ab933ca74cbd4abb5c242
Cecilio Morales <cecilio.morales@leniolabs.com>
2023-02-15 21:37:23 -0500

    Sanctum2

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:9000,127.0.0.1,127.0.0.1:8000,::1,*.artisanburgers.pe',
+        'localhost,localhost:9000,127.0.0.1,127.0.0.1:8000,::1,uchip.artisanburgers.pe',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
