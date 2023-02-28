5a096fbec86b3cafb452790f7d83868d64715fe5
Cecilio Morales <cecilio.morales@leniolabs.com>
2023-02-28 14:22:59 -0500

    Fixes for Broadcast Websockets

--- config/websockets.php
+++ config/websockets.php
@@ -31,6 +31,7 @@ return [
             'capacity' => null,
             'enable_client_messages' => false,
             'enable_statistics' => true,
+            'encrypted' => true,
         ],
     ],
 
@@ -116,18 +117,21 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        //'local_cert' => "/etc/pki/tls/certs/api.artisanburgers.pe.cert",
+        'local_cert' => null,
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        //'local_pk' =>  "/etc/pki/tls/private/api.artisanburgers.pe.key",
+        'local_pk' => null,
+        
 
         /*
          * Passphrase for your local_cert file.
          */
-        'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
+        'passphrase' => null,
     ],
 
     /*
